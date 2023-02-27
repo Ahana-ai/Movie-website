ae9febb668ffe53663720207aeefd149d85fc772
Ahana <ahanade100@gmail.com>
2023-02-27 21:17:22 +0530

    Search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../context";
 
 const Search = () => {
+  const { query, setQuery, isError } = useContext(AppContext);
   return (
     <>
-      <h1>Search</h1>
+      <section>
+        <h2>Search your favourite movie</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div>
+            <input
+              type="text"
+              placeholder="Search here"
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+            />
+          </div>
+        </form>
+        <div>
+          <p>{isError && isError.msg}</p>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Search
-
+export default Search;
--- src/context.jsx
+++ src/context.jsx
@@ -1,43 +1,49 @@
 import { createContext, useEffect, useState } from "react";
 const AppContext = createContext();
 
-const API_URL = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_APP_API_KEY}&s=jumanji`
+const API_URL = `http://www.omdbapi.com/?apikey=${
+  import.meta.env.VITE_APP_API_KEY
+}`;
 
-const AppProvider = ({children}) => {
-    //useState to fetch items from the 'Search' array from api
-    const [movie, setMovie] = useState([]);
-    //useState to show error
-    const [isError, setIsError] = useState({ show: "false", msg: "" });
+const AppProvider = ({ children }) => {
+  //useState to fetch items from the 'Search' array from api
+  const [movie, setMovie] = useState([]);
+  //useState to show error
+  const [isError, setIsError] = useState({ show: "false", msg: "" });
+  //useState to find the movie in search box
+  const [query, setQuery] = useState("jumanji");
 
-    //Function to call the api url
-    const getMovies = async (url) => {
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            console.log(data);
+  //Function to call the api url
+  const getMovies = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
 
-            //when the response to promise is true then set the movie
-            if(data.Response === "True"){
-                setMovie(data.Search);
-            } else {
-                setIsError({
-                    show: true,
-                    msg: data.error
-                })
-            }
-        } catch (error) {
-            console.log(error);
-        }
+      //when the response to promise is true then set the movie
+      if (data.Response === "True") {
+        setMovie(data.Search);
+      } else {
+        setIsError({
+          show: true,
+          msg: data.error,
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    //useEffect to get the data repeatedly
-    useEffect(() => {
-        getMovies(API_URL);
-    },[]);
+  //useEffect to get the data repeatedly
+  useEffect(() => {
+    getMovies(`${API_URL}&s=${query}`);
+  },[query]);
 
-    return <AppContext.Provider value={{ movie, isError }}>
-        {children}
+  return (
+    <AppContext.Provider value={{ movie, isError, query, setQuery }}>
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
 export { AppContext, AppProvider };
